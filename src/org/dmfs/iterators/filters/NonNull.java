4b7523256fe9bf72881f3925a0b1618859cd3665
Marten Gajda <marten@dmfs.org>
2016-06-13 14:36:36 +0200

    Add NonNull filter that removes all null elements. Add missing Javadoc.

--- src/org/dmfs/iterators/filters/NonNull.java
+++ src/org/dmfs/iterators/filters/NonNull.java
@@ -1,57 +1,45 @@
 /*
  * Copyright (C) 2016 Marten Gajda <marten@dmfs.org>
  *
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package org.dmfs.iterators.filters;
 
-import org.dmfs.iterators.AbstractFilteredIterator;
 import org.dmfs.iterators.AbstractFilteredIterator.IteratorFilter;
 
 
 /**
- * An {@link IteratorFilter} that filters elements by a list of known elements to include in the result.
+ * An {@link IteratorFilter} that removes all <code>null</code> elements.
  * 
  * @author Marten Gajda <marten@dmfs.org>
- * 
- * @param <E>
- *            The element type.
  */
-public class AnyOf<E> implements AbstractFilteredIterator.IteratorFilter<E>
+public class NonNull<T> implements IteratorFilter<T>
 {
-	private final E[] mIncludes;
+	public final static IteratorFilter<Object> INSTANCE = new NonNull<Object>();
 
 
-	@SafeVarargs
-	public AnyOf(E... includes)
+	@SuppressWarnings("unchecked")
+	public static <T> NonNull<T> instance()
 	{
-		mIncludes = includes.clone();
+		return (NonNull<T>) INSTANCE;
 	}
 
 
 	@Override
-	public boolean iterate(E element)
+	public boolean iterate(T element)
 	{
-		for (E include : mIncludes)
-		{
-			if (include == null && element == null || include != null && include.equals(element))
-			{
-				return true;
-			}
-		}
-		return false;
+		return element != null;
 	}
-
 }
--- src/org/dmfs/iterators/filters/NoneOf.java
+++ src/org/dmfs/iterators/filters/NoneOf.java
@@ -1,8 +1,33 @@
+/*
+ * Copyright (C) 2016 Marten Gajda <marten@dmfs.org>
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.dmfs.iterators.filters;
 
 import org.dmfs.iterators.AbstractFilteredIterator;
+import org.dmfs.iterators.AbstractFilteredIterator.IteratorFilter;
 
 
+/**
+ * An {@link IteratorFilter} that filters elements by a list of known elements to exclude from the result.
+ * 
+ * @author Marten Gajda <marten@dmfs.org>
+ *
+ * @param <E> The element type.
+ */
 public class NoneOf<E> implements AbstractFilteredIterator.IteratorFilter<E>
 {
 	private final E[] mExcludes;
@@ -11,7 +36,7 @@ public class NoneOf<E> implements AbstractFilteredIterator.IteratorFilter<E>
 	@SafeVarargs
 	public NoneOf(E... excludes)
 	{
-		mExcludes = excludes;
+		mExcludes = excludes.clone();
 	}
 
 
